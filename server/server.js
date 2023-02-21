a5cbc387d3a3016a423120c2c709fcb7e869b6f1
scottsc0de <scott5902@gmail.com>
2023-02-21 11:58:30 -0500

    command src

--- server/server.js
+++ server/server.js
@@ -10,3 +10,6 @@ app.use(express.json());
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`I'm listening`));
+
+// package.json scripts object for commands
+// PWA class module 14 under package.json for commands
